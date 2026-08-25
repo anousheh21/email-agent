e0181bbbc4474fd1612fb870cf3f217950a10f3f
anousheh21 <anousheh1@gmail.com>
2026-08-25 08:11:52 +0100

    tool call, but doesnt work bc auth yet to be implemented

--- src/client.ts
+++ src/client.ts
@@ -8,6 +8,15 @@ await client.connect(transport);
 
 // List out tools
 const { tools } = await client.listTools();
-for (const tool of tools) {
-    console.log(tool.name);
-}
+console.log(tools.map(tool => tool.name));
+
+// Not working because you have to implement auth!
+const result = await client.callTool({ name: 'search_threads', arguments: { query: 'from: google '}});
+console.log(result.content);
+
+// Might be worth doing another tool call here, to double check that the auth persists
+
+
+// If it makes you log in every time you run it, can you cache somewhere to prevent from having to do this every time in development (so it logs you in automatically)? 
+
+// Also, for writing tests, how is it going to log in so that it can check the tools are running properly?
