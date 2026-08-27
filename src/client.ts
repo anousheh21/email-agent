1b9799747ac0d281fe3f79127946c197d29edbb6
anousheh21 <anousheh1@gmail.com>
2026-08-27 16:09:03 +0100

    started implementing the gmail oauth provider

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,45 @@
-import { Client, StreamableHTTPClientTransport } from "@modelcontextprotocol/client";
+import "dotenv/config";
+import { Client, StreamableHTTPClientTransport, UnauthorizedError } from "@modelcontextprotocol/client";
+import type { OAuthClientMetadata, OAuthClientProvider } from "@modelcontextprotocol/client";
 
+const clientId = getRequiredEnv(process.env.GMAIL_AUTH_CLIENT_ID);
+const clientSecret = getRequiredEnv(process.env.GMAIL_AUTH_CLIENT_SECRET);
+
+if (!clientId || !clientSecret) {
+    throw new Error("Missing OAuth credentials");
+}
+
+class GmailOAuthProvider implements OAuthClientProvider {
+    readonly redirectUrl = 'http://localhost:8090/callback';
+    readonly clientMetadata: OAuthClientMetadata = {
+        client_name: 'Email Agent',
+        redirect_uris: ['http://localhost:8090/callback'],
+        application_type: 'web application'
+    };
+
+    clientInformation() {
+        return {
+            client_id: clientId,
+            client_secret: clientSecret
+        }
+    }
+    
+}
+
+
+const provider = new GmailOAuthProvider();
 const client = new Client({ name: 'gmail-client', version: '1.0.0' });
 
-const transport = new StreamableHTTPClientTransport(new URL('https://gmailmcp.googleapis.com/mcp/v1'));
+const transport = new StreamableHTTPClientTransport(new URL('https://gmailmcp.googleapis.com/mcp/v1'), { authProvider: provider });
 
-await client.connect(transport);
+try {
+    await client.connect(transport);
+} catch (error) {
+    if (!(error instanceof UnauthorizedError)) throw error;
+}
+
+// Add in the 'finish the flow from the callback' url here
+
 
 // List out tools
 const { tools } = await client.listTools();
@@ -13,6 +48,20 @@ console.log(tools.map(tool => tool.name));
 // Not working because you have to implement auth!
 const result = await client.callTool({ name: 'search_threads', arguments: { query: 'from: google '}});
 console.log(result.content);
+
+function getRequiredEnv(name: string | undefined): string {
+    if (!name) {
+        throw new Error(`Missing environment variable: ${name}`)
+    }
+
+    const value = process.env[name];
+
+    if (!value) {
+        throw new Error(`Missing environment variable: ${name}`);
+    }
+
+    return value;
+}
 
 // Might be worth doing another tool call here, to double check that the auth persists
 
